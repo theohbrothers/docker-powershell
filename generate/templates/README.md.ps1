c3c8833cbac14dfd0cb91c3821124a1226be7f7d
Leo <leonardjonathanoh@live.com>
2019-10-29 21:20:48 +0000

    Update readme with github-actions badge

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,7 +1,7 @@
 @"
 # docker-powershell
 
-[![pipeline status](https://gitlab.com/leojonathanoh/docker-powershell/badges/dev/pipeline.svg)](https://gitlab.com/leojonathanoh/docker-powershell/commits/dev)
+[![github-actions](https://github.com/leojonathanoh/docker-powershell/workflows/build/badge.svg)](https://github.com/leojonathanoh/docker-powershell/actions)
 [![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-powershell)](https://github.com/leojonathanoh/docker-powershell/releases/)
 [![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-powershell/latest)](https://hub.docker.com/r/leojonathanoh/docker-powershell)
 [![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-powershell/latest)](https://hub.docker.com/r/leojonathanoh/docker-powershell)
