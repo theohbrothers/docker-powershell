193b96c17a84256b7493f5820a93629522d92302
Leo <leonardjonathanoh@live.com>
2019-11-16 15:51:21 +0000

    Rename repository owner to theohbrothers

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -1,10 +1,10 @@
 @"
 # docker-powershell
 
-[![github-actions](https://github.com/leojonathanoh/docker-powershell/workflows/build/badge.svg)](https://github.com/leojonathanoh/docker-powershell/actions)
-[![github-tag](https://img.shields.io/github/tag/leojonathanoh/docker-powershell)](https://github.com/leojonathanoh/docker-powershell/releases/)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/leojonathanoh/docker-powershell/latest)](https://hub.docker.com/r/leojonathanoh/docker-powershell)
-[![docker-image-layers](https://img.shields.io/microbadger/layers/leojonathanoh/docker-powershell/latest)](https://hub.docker.com/r/leojonathanoh/docker-powershell)
+[![github-actions](https://github.com/theohbrothers/docker-powershell/workflows/build/badge.svg)](https://github.com/theohbrothers/docker-powershell/actions)
+[![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-powershell)](https://github.com/theohbrothers/docker-powershell/releases/)
+[![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-powershell/latest)](https://hub.docker.com/r/theohbrothers/docker-powershell)
+[![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-powershell/latest)](https://hub.docker.com/r/theohbrothers/docker-powershell)
 
 Docker image based on [mcr.microsoft.com/powershell](https://hub.docker.com/r/microsoft/powershell/)
 
