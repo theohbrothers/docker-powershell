c21a10dbfcfdc302268a1fb262a0aebd08b2b050
Leo <leonardjonathanoh@live.com>
2019-11-16 21:46:43 +0000

    Remove unused VARIANTS_VERSION variable in generate/definitions/VARIANTS.ps1

--- generate/definitions/VARIANTS.ps1
+++ generate/definitions/VARIANTS.ps1
@@ -1,5 +1,4 @@
 # Docker image variants' definitions
-$VARIANTS_VERSION = "1.0.1"
 $VARIANTS = @(
     @{
         tag = 'curl-ubuntu18.04'
@@ -29,7 +28,6 @@ $VARIANTS = @(
 
 # Docker image variants' definitions (shared)
 $VARIANTS_SHARED = @{
-    version = $VARIANTS_VERSION
     buildContextFiles = @{
         templates = @{
             'Dockerfile' = @{
