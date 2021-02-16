634be4a1c041ac7d15697c582bf4248c98668fb6
Leonard Jonathan Oh <leonardjonathanoh@live.com>
2021-02-16 14:55:10 +0000

    Docs: Replace microbadger image size and image layers badges with shields.io docker image size badge in readme

--- generate/templates/README.md.ps1
+++ generate/templates/README.md.ps1
@@ -3,8 +3,7 @@
 
 [![github-actions](https://github.com/theohbrothers/docker-powershell/workflows/ci-master-pr/badge.svg)](https://github.com/theohbrothers/docker-powershell/actions)
 [![github-tag](https://img.shields.io/github/tag/theohbrothers/docker-powershell)](https://github.com/theohbrothers/docker-powershell/releases/)
-[![docker-image-size](https://img.shields.io/microbadger/image-size/theohbrothers/docker-powershell/latest)](https://hub.docker.com/r/theohbrothers/docker-powershell)
-[![docker-image-layers](https://img.shields.io/microbadger/layers/theohbrothers/docker-powershell/latest)](https://hub.docker.com/r/theohbrothers/docker-powershell)
+[![docker-image-size](https://img.shields.io/docker/image-size/theohbrothers/docker-powershell/latest)](https://hub.docker.com/r/theohbrothers/docker-powershell)
 
 Dockerized ``powershell``, based on [mcr.microsoft.com/powershell](https://hub.docker.com/r/microsoft/powershell/), with useful tools.
 
